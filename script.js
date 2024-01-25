5410b16becf6858e2d40c2a02c85a47e317f1742
shiwirockztar <shiwirockztar@gmail.com>
2024-01-24 19:22:57 -0500

    add stop button

--- script.js
+++ script.js
@@ -1,23 +1,45 @@
-const $button = document.querySelector("button");
+const $button = document.querySelector("#startButton");
+const $stopButton = document.querySelector("#stopButton");
+
+let media;
+let mediarecorder;
 
 $button.addEventListener("click", async () => {
-  const media = await navigator.mediaDevices.getDisplayMedia({
-    video: { frameRate: { ideal: 30 } },
-  });
-  const mediarecorder = new MediaRecorder(media, {
-    mimeType: "video/webm;codecs=vp8,opus",
-  });
-  mediarecorder.start();
+  try {
+    media = await navigator.mediaDevices.getDisplayMedia({
+      video: { frameRate: { ideal: 30 } },
+      audio: true,
+    });
 
-  const [video] = media.getVideoTracks();
-  video.addEventListener("ended", () => {
-    mediarecorder.stop();
-  });
+    mediarecorder = new MediaRecorder(media, {
+      mimeType: "video/webm;codecs=vp8,opus",
+    });
+    mediarecorder.start();
 
-  mediarecorder.addEventListener("dataavailable", (e) => {
-    const link = document.createElement("a");
-    link.href = URL.createObjectURL(e.data);
-    link.download = "captura.webm";
-    link.click();
-  });
+    const [video] = media.getVideoTracks();
+    video.addEventListener("ended", () => {
+      stopRecording();
+    });
+
+    mediarecorder.addEventListener("dataavailable", (e) => {
+      const link = document.createElement("a");
+      link.href = URL.createObjectURL(e.data);
+      link.download = "captura.webm";
+      link.click();
+    });
+  } catch (error) {
+    console.error("Error al obtener medios: ", error);
+  }
 });
+
+$stopButton.addEventListener("click", () => {
+  stopRecording();
+});
+
+function stopRecording() {
+  if (mediarecorder && media) {
+    mediarecorder.stop();
+    const [video] = media.getVideoTracks();
+    video.stop();
+  }
+}
